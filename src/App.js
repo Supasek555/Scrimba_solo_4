28acee0f5519b7cbc45f02ae36352ae8bd018eaa
Supasek Dhanabordeephat <Supasekmai@gmail.com>
2022-06-16 22:00:12 +0700

    Add second Page

--- src/App.js
+++ src/App.js
@@ -7,6 +7,22 @@ export default function App(){
 
   const [isPlayed,setIsPlayed] = React.useState(false);
 
+  const [quiz,setQuiz] = React.useState({})
+
+
+
+       
+
+        React.useEffect(function(){
+            fetch("https://opentdb.com/api.php?amount=5&category=23&difficulty=easy&type=multiple")
+        .then(res => res.json())
+        .then(data => setQuiz(data.results))
+
+         console.log(JSON.stringify(quiz[0]))
+        },[isPlayed])
+
+        
+
   function play(){
     setIsPlayed(!isPlayed)
 
@@ -29,6 +45,8 @@ export default function App(){
 
           <button onClick={play}>Start Quiz</button>
         </div>
+
+        <Quiz displayed={isPlayed}/>
         
       
       
--- src/Components/Quiz.js
+++ src/Components/Quiz.js
@@ -1,11 +1,18 @@
 import React from "react"
 
-export default function Quiz(){
-    return(
-    
-    
-    <div className="quiz">
+export default function Quiz(props){
 
+
+    
+
+
+    return(
+        
+    
+    
+    <div className={props.displayed ? "quiz-show" : "quiz-off"}>
+        <h1>Jum Reap Suo</h1>
+        <button>Check Answer</button>
     </div>
     
     
