f874b1afd7d50d893fcefde3347bdc524e7e22ca
Supasek Dhanabordeephat <Supasekmai@gmail.com>
2022-06-16 14:46:08 +0700

    First Page Set Up

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react"
+import "./style.css"
+import Quiz from "./Components/Quiz";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+export default function App(){
+
+  const [isPlayed,setIsPlayed] = React.useState(false);
+
+  function play(){
+    setIsPlayed(!isPlayed)
+
+    console.log(isPlayed)
+  }
+
+
+
+  return(
+    <div className="app">
+
+        
+
+        <div className={isPlayed ? "reception-off" : "reception"} >
+          <div className="reception_text">
+            <h1>Jum Reap Suo</h1>
+            <h3>Mian Soup Hei?</h3>
+          </div>
+         
+
+          <button onClick={play}>Start Quiz</button>
+        </div>
+        
+      
+      
     </div>
-  );
+  )
+  
+  
 }
 
-export default App;
+
---
+++ src/Components/Quiz.js
@@ -0,0 +1,13 @@
+import React from "react"
+
+export default function Quiz(){
+    return(
+    
+    
+    <div className="quiz">
+
+    </div>
+    
+    
+    )
+}
