26422af759a477a13f015facbd774d63f816c34f
Supasek Dhanabordeephat <Supasekmai@gmail.com>
2022-06-21 15:59:59 +0700

    Ampping all the answers into a quiz page

--- src/Components/Quiz.js
+++ src/Components/Quiz.js
@@ -5,8 +5,16 @@ export default function Quiz(props){
 
     
     const quizElement = props.quizList.map(myQuiz => (
+
         
-        <QuizElement quizQuestion ={myQuiz.question} />
+        
+        <QuizElement 
+
+        quizQuestion ={myQuiz.question} 
+        correct={myQuiz.correct_answer} 
+        incorrect={myQuiz.incorrect_answers}
+        
+        />
     ))
 
 
@@ -16,11 +24,15 @@ export default function Quiz(props){
     
     <div className="quiz-show">
         
-       
+       <div className="quiz_question">
         {quizElement}
+       </div>
+       
 
-
-        <button className="normal">Check Answer</button>
+        <div className="button_section">
+            <button className="normal">Check Answer</button>
+        </div>
+ 
     </div>
     
     
--- src/Components/QuizElement.js
+++ src/Components/QuizElement.js
@@ -2,7 +2,35 @@ import React from "react"
 
 export default function QuizElement(props){
 
+    const correct_one = [props.correct];
 
+    const answer_array = correct_one.concat(props.incorrect)
+
+    const random_array = shuffle(answer_array)
+
+
+  
+
+    function shuffle(array) {
+        let currentIndex = array.length,  randomIndex;
+      
+        // While there remain elements to shuffle.
+        while (currentIndex !== 0) {
+      
+          // Pick a remaining element.
+          randomIndex = Math.floor(Math.random() * currentIndex);
+          currentIndex--;
+      
+          // And swap it with the current element.
+          [array[currentIndex], array[randomIndex]] = [
+            array[randomIndex], array[currentIndex]];
+        }
+      
+        return array;
+      }
+
+    
+      
 
     return(
         
@@ -13,10 +41,10 @@ export default function QuizElement(props){
         <h2>{props.quizQuestion}</h2>
         
         <div className="answer_grp">
-            <button className="answer">1</button>
-            <button className="answer">2</button>
-            <button className="answer">3</button>
-            <button className="answer">4</button>
+            <button className="answer">{random_array[0]}</button>
+            <button className="answer">{random_array[1]}</button>
+            <button className="answer">{random_array[2]}</button>
+            <button className="answer">{random_array[3]}</button>
         </div>
     </div>
     
